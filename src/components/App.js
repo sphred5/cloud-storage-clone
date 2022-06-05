aa9dd0c87adafc47ce22a36b81f38aad94141d63
Steven Haye <sphred5@gmail.com>
2022-06-05 11:59:59 -0700

    built out authentication, private routes next

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<Dashboard/>}/>
+              <Route path="/signup" element={<Signup />}/>
+              <Route path="/login" element={<Login />}/>
+            </Routes>
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
